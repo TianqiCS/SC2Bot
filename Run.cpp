4ca3620c5d3f355da147224e5862bde237372867
drunkHatch <yqzhang1104@gmail.com>
2018-11-12 20:29:14 -0700

    setting demo environment PLEASE do not change commented main function in Run.cpp Thanks

--- Run.cpp
+++ Run.cpp
@@ -8,26 +8,28 @@
 #include <iostream>
 
 #ifdef DEBUG
+
 int main(int argc, char* argv[])
 {
-	/*Feeder bot;
+	/*
+	Feeder bot;
 	sc2::Coordinator coordinator;
 	if (!coordinator.LoadSettings(argc, argv))
 	{
 		std::cout << "Unable to find or parse settings." << std::endl;
 		return 1;
 	}
-	coordinator.SetWindowSize(1024, 768);
+	coordinator.SetWindowSize(1920, 1080);
 	coordinator.SetStepSize(100); // speed
 	coordinator.SetRealtime(false);
 	coordinator.SetMultithreaded(true);
 	coordinator.SetParticipants({
 		CreateParticipant(sc2::Race::Terran, &bot),
 		//sc2::PlayerSetup(sc2::PlayerType::Observer,Util::GetRaceFromString(enemyRaceString)),
-		CreateComputer(sc2::Race::Random, Easy)
+		CreateComputer(sc2::Race::Random, VeryHard)
 	});
 	// Start the game.
-	//coordinator.LaunchStarcraft();
+	coordinator.LaunchStarcraft();
 	coordinator.StartGame("/CactusValleyLE.SC2Map");
 	//coordinator.StartGame("Interloper LE");
 
@@ -40,6 +42,45 @@ int main(int argc, char* argv[])
 	RunBot(argc, argv, new Feeder(), sc2::Race::Terran);
 	return 0;
 }
+
+// This main function is for quick local demo only
+// please DO NOT EDIT/DELETE
+
+/*
+int main(int argc, char* argv[])
+{
+	Feeder bot;
+	sc2::Coordinator coordinator;
+	if (!coordinator.LoadSettings(argc, argv))
+	{
+		std::cout << "Unable to find or parse settings." << std::endl;
+		return 1;
+	}
+	coordinator.SetWindowSize(1920, 1080);
+	coordinator.SetStepSize(100); // speed
+	coordinator.SetRealtime(false);
+	coordinator.SetMultithreaded(true);
+	coordinator.SetParticipants({
+		CreateParticipant(sc2::Race::Terran, &bot),
+		//sc2::PlayerSetup(sc2::PlayerType::Observer,Util::GetRaceFromString(enemyRaceString)),
+		CreateComputer(sc2::Race::Random, VeryHard)
+		});
+	// Start the game.
+	coordinator.LaunchStarcraft();
+	coordinator.StartGame("/CactusValleyLE.SC2Map");
+	//coordinator.StartGame("Interloper LE");
+
+
+	// Step forward the game simulation.
+	while (coordinator.Update())
+	{
+	}
+
+	//RunBot(argc, argv, new Feeder(), sc2::Race::Terran);
+	return 0;
+}
+
+*/
 #else
 
 int main(int argc, char* argv[])
